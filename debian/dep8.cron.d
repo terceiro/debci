2a094afc8fc8b7f88d4dd0bb37f1b8f4360b7b01
Antonio Terceiro <terceiro@debian.org>
2014-01-04 12:20:47 -0300

    Disable cron job for now

--- debian/dep8.cron.d
+++ debian/dep8.cron.d
@@ -1,4 +1,4 @@
 #
 # Regular cron jobs for the dep8 package
 #
-13 */6 * * *	root	[ -x /usr/sbin/dep8-run ] && /usr/sbin/dep8-run
+# 13 */6 * * *	root	[ -x /usr/sbin/dep8-run ] && /usr/sbin/dep8-run
