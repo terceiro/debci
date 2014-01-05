7bfb24791e7c2204556e30ad837c4a2990e99591
Antonio Terceiro <terceiro@debian.org>
2014-01-05 15:49:13 -0300

    going on with consolidation/reuse improvements

--- lib/environment.sh
+++ lib/environment.sh
@@ -10,8 +10,9 @@ fi
 
 dep8_suite='unstable' # FIXME
 
-dep8_data_dir="${dep8_base_dir}/data"
+dep8_data_dir=$(readlink -f "${dep8_base_dir}/data")
 dep8_packages_dir="${dep8_data_dir}/packages"
+dep8_status_dir="${dep8_data_dir}/status"
 
 dep8_config_dir="${dep8_base_dir}/config"
 
@@ -20,3 +21,5 @@ dep8_gnupg_dir="${dep8_base_dir}/gnupg"
 dep8_chroots_dir="${dep8_base_dir}/chroots"
 dep8_chroot_name="dep8-${dep8_suite}"
 dep8_chroot_path="${dep8_chroots_dir}/${dep8_suite}"
+
+dep8_user=$(stat -c %U "${dep8_data_dir}")
