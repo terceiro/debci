33ca1cb7397fb53a2e5261971ea78c25e41d4dd5
Antonio Terceiro <terceiro@debian.org>
2014-02-25 10:17:04 -0300

    Add test for JSON validity
    also tests can now be written in any language. we just run all
    executables under test/

--- test/runall.sh
+++ test/runall.sh
@@ -7,19 +7,19 @@ report() {
   local test_name="$2"
   local result="$3"
   if test -t 1; then
-    printf "\033[38;5;${color}m%-70s %s\033[m\n" "$test_name" "$result"
+    printf "\033[38;5;${color}m%-40s %s\033[m\n" "$test_name" "$result"
   else
-    printf "%-70s %s\n" "$test_name" "$result"
+    printf "%-40s %s\n" "$test_name" "$result"
   fi
 }
 
 tests=0
 passed=0
 failed=0
-for test_script in test/test_*.sh; do
+for test_script in $(find 'test/' -type f -executable); do
   tests=$(($tests + 1))
   test_name=$(basename $test_script)
-  if sh $test_script; then
+  if $test_script; then
     report 2 $test_name PASS
     passed=$(($passed + 1))
   else
