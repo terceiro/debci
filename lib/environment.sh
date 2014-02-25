57a372ffaf177c40a7079908836136eb5dac5fdd
Antonio Terceiro <terceiro@debian.org>
2014-02-24 23:58:21 -0300

    Rewrite strategy for processing all packages
    replace usage of make with parallel from moreutils

--- lib/environment.sh
+++ lib/environment.sh
@@ -4,7 +4,7 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 
 if [ -z "$debci_base_dir" ]; then
-  if [ -f scripts/process-all-packages -a -f lib/environment.sh ]; then
+  if [ -f lib/environment.sh ]; then
     debci_base_dir="$(pwd)"
   else
     echo "E: no \$debci_base_dir not set!"
