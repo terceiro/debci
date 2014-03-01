510a88e5d98e1c14a375ecac42bb5408699bc7db
Antonio Terceiro <terceiro@debian.org>
2014-03-01 15:16:40 -0300

    Add support for blaming dependencies on failures

--- test/test_helper.sh
+++ test/test_helper.sh
@@ -1,9 +1,10 @@
 # set TMPDIR for tests
 export TMPDIR=$(mktemp -d)
-__tmpdir_cleanup() {
-  rm -rf "$TMPDIR"
+oneTimeTearDown() {
+  if [ "${DEBCI_DEBUG:-false}" = 'false' ]; then
+    rm -rf $TMPDIR
+  fi
 }
-trap __tmpdir_cleanup INT TERM EXIT
 
 # DEP-8/autopkgtest support: only use local binaries when not running under a
 # DEP-8 runner, when you are supposed to test the installed package
