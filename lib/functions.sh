1eed2fa4c92daff0dc98f01613663854deca8523
Martin Pitt <martin.pitt@ubuntu.com>
2014-04-05 13:25:12 +0100

    Move report_status() into lib/functions.sh
    We'll need it in more places than just process-package.

--- lib/functions.sh
+++ lib/functions.sh
@@ -46,6 +46,35 @@ log() {
   fi
 }
 
+
+report_status() {
+  local pkg="$1"
+  local status="$2"
+  if [ -t 1 ]; then
+    case "$status" in
+      skip)
+        color=8
+        ;;
+      pass)
+        color=2
+        ;;
+      fail)
+        color=1
+        ;;
+      tmpfail)
+        color=3
+        ;;
+      *)
+        color=5 # should never get here though
+        ;;
+    esac
+    log "${pkg} \033[38;5;${color}m${status}\033[m"
+  else
+    log "$pkg" "$status"
+  fi
+}
+
+
 command_available() {
   which "$1" >/dev/null 2>/dev/null
 }
