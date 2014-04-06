1e76bcf3e36877d3456ccaf1690ee89b83f9844d
Antonio Terceiro <terceiro@debian.org>
2014-04-06 10:50:25 -0300

    Fix tests afte renaming `debci` to `debci-batch`

--- test/test_basics.sh
+++ test/test_basics.sh
@@ -3,12 +3,12 @@
 . $(dirname $0)/test_helper.sh
 
 test_everything_passes() {
-  result_pass debci
+  result_pass debci-batch
   assertEquals "pass" "$(debci-status -l | awk '{print($2)}' | uniq)"
 }
 
 test_everything_fails() {
-  result_fail debci
+  result_fail debci-batch
   assertEquals 'fail' "$(debci-status -l | awk '{print($2)}' | uniq)"
 }
 
