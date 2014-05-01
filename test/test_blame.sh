600bdffd1ee88e15ab6af89b8ef94773f12eb358
Antonio Terceiro <terceiro@debian.org>
2014-05-01 19:05:52 -0300

    debci-generate-index: accept duration as a command line switch

--- test/test_blame.sh
+++ test/test_blame.sh
@@ -10,7 +10,7 @@ process() {
   DEBCI_FAKE_DEPS="$dependencies" \
     faketime +${__day}days \
     debci-test "$pkg" --quiet
-    debci-generate-index -q 0
+    debci-generate-index -q --duration 0
   __day=$(($__day + 1))
 }
 
