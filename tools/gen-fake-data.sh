13114059a7315d278e591a42d55c06cc6796c5b7
Antonio Terceiro <terceiro@debian.org>
2014-05-16 19:13:07 -0300

    tools/gen-fake-data.sh: fix for new CLI

--- tools/gen-fake-data.sh
+++ tools/gen-fake-data.sh
@@ -5,7 +5,7 @@ set -eu
 debci_status_dir=$(sh -c '. lib/environment.sh ; echo $debci_status_dir')
 
 for n in $(seq 1 15); do
-  faketime +${n}days ./bin/debci --backend fake -j 2
+  faketime +${n}days ./bin/debci batch --backend fake -j 2
   # fake the duration with a "random" number of seconds up to 10h
   status_file="${debci_status_dir}/status.json"
   duration=$(( $(sha256sum "$status_file" | sed ' s/[^0-9]//g; s/^0\+//; s/^\([0-9]\{0,6\}\).*/\1/g;') % 36000 ))
