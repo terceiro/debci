ecf199dd94e8bfae9f7b65dccf5fa1fd3a6f09ba
Antonio Terceiro <terceiro@debian.org>
2014-05-16 19:19:12 -0300

    debian-status: avoid generating invalid JSON

--- test/test_basics.sh
+++ test/test_basics.sh
@@ -12,4 +12,12 @@ test_everything_fails() {
   assertEquals 'fail' "$(debci status -l | awk '{print($2)}' | uniq)"
 }
 
+test_packages_without_runs_yet() {
+  result_pass debci batch
+  find $debci_data_basedir -type d -name rake | xargs rm -rf
+  debci generate-index
+  find $debci_data_basedir -name packages.json | xargs cat | json_pp -f json -t json > /dev/null
+  assertEquals 0 $?
+}
+
 . shunit2
