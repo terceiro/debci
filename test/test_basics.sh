65a926feefd9ab8b219184e7e8845fced4aaa180
Martin Pitt <martin.pitt@ubuntu.com>
2014-05-20 16:51:32 +0200

    Fix debci-status -l if there is just one package
    We were just outputting the status, instead of "packagename status", as we
    would expect with -l/--all.
    
    Add reproducer to test suite, and some checks that the status output is as
    expected for multiple packages, too.

--- test/test_basics.sh
+++ test/test_basics.sh
@@ -4,12 +4,22 @@
 
 test_everything_passes() {
   result_pass debci batch
-  assertEquals "pass" "$(debci status -l | awk '{print($2)}' | uniq)"
+  status=$(debci status -l)
+  assertEquals "pass" "$(echo "$status" | awk '{print($2)}' | uniq)"
+
+  # check validity of debci-status format
+  echo "$status" | grep -q '^ruby *pass$' || fail "invalid format:\n$status"
+  echo "$status" | grep -q '^rake *pass$' || fail "invalid format:\n$status"
 }
 
 test_everything_fails() {
   result_fail debci batch
-  assertEquals 'fail' "$(debci status -l | awk '{print($2)}' | uniq)"
+  status=$(debci status -l)
+  assertEquals "fail" "$(echo "$status" | awk '{print($2)}' | uniq)"
+
+  # check validity of debci-status format
+  echo "$status" | grep -q '^ruby *fail$' || fail "invalid format:\n$status"
+  echo "$status" | grep -q '^rake *fail$' || fail "invalid format:\n$status"
 }
 
 test_packages_without_runs_yet() {
@@ -18,6 +28,12 @@ test_packages_without_runs_yet() {
   debci generate-index
   find $debci_data_basedir -name packages.json | xargs cat | json_pp -f json -t json > /dev/null
   assertEquals 0 $?
+}
+
+test_single_package() {
+  echo "mypkg" > $debci_config_dir/whitelist
+  result_pass debci batch
+  assertEquals "mypkg pass" "$(debci status -l)"
 }
 
 . shunit2
