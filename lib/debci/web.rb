43f4eb5027ca9a95f11191e5f1e408ed02df4bff
Brandon Fairchild <csbit32@gmail.com>
2014-05-20 11:15:49 -0400

    Set the public directory for Sinatra

--- lib/debci/web.rb
+++ lib/debci/web.rb
@@ -5,7 +5,8 @@ module Debci
     class Web < Sinatra::Base
             
         set :views, root + "/web/views" 
-        
+        set :public_folder, File.dirname("config.ru") + "/public"
+
         get '/' do
             erb :index
         end
