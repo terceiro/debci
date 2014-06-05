42f4c9471d7381cfa09c1a95279f428ca059d900
Brandon Fairchild <csbit32@gmail.com>
2014-06-05 19:52:13 -0400

    lib/debci/web.rb: Move setting of the public folder to the development block

--- lib/debci/web.rb
+++ lib/debci/web.rb
@@ -6,13 +6,15 @@ module Debci
     class Web < Sinatra::Base
 
         set :views, root + "/web/views"
-        set :public_folder, File.dirname("config.ru") + "/public"
 
         get '/' do
             erb :index
         end
 
         if development?
+
+            set :public_folder, File.dirname("config.ru") + "/public"
+
             get '/doc/' do
                 send_file File.join(settings.public_folder, '/doc/index.html')
             end
