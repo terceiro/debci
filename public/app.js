ad3009da0f40edeba63de68eabe9479f19ae6ec6
Brandon Fairchild <csbit32@gmail.com>
2014-08-14 20:33:30 -0400

    public/app.js: Remove code that is no longer needed

--- public/app.js
+++ public/app.js
@@ -1,9 +1,6 @@
 jQuery(function($) {
 
-  // FIXME generalize this later
-  var PACKAGES_DIR = '/data/packages/unstable/amd64';
   var PACKAGES_HTML_DIR = '/data/.html/packages';
-  var AUTOPKGTEST_DIR = '/data/autopkgtest/unstable/amd64';
   var STATUS_DIR = '/data/status/';
   var STATUS_HTML_DIR = '/data/.html/status';
 
@@ -125,28 +122,6 @@ jQuery(function($) {
             }
           });
 
-          var max_hours = Math.round(max_duration / 3600) + 1;
-          var step = Math.ceil(max_hours / 10);
-          max_hours = step * 10;
-          var duration_ticks = [];
-          for (var i = 0; i <= max_hours; i = i + step) {
-            duration_ticks.push([i * 3600, i + 'h']);
-          }
-          $.plot('#chart-run-duration', [duration], {
-            series: {
-              bars: {
-                show: true
-             }
-            },
-            xaxis: {
-              mode: 'time',
-            },
-            yaxis: {
-              min: 0,
-              ticks: duration_ticks
-            }
-
-          });
         })
       })
     })
@@ -223,72 +198,6 @@ jQuery(function($) {
         $('#package-select li').hide();
       }
   });
-
-  function display_details(pkg) {
-    var pkg_dir = pkg.replace(/^((lib)?.)/, "$1/$&");
-    var url = PACKAGES_DIR + '/' + pkg_dir + '/history.json';
-
-    var $target = $('#package-details')
-    $target.html('');
-    $.get(url, function(history) {
-      var feed_url = 'data/feeds/' + pkg_dir + '.xml';
-      var feed = "&nbsp;<a href=\"" + feed_url + "\" title=\"Atom feed for " + pkg + "\"><span class='fa fa-rss'></span></a>";
-      $target.append("<h1>" + pkg + feed + "</h1>");
-
-      if (history[0]) {
-        $('#package-details h1').addClass(history[0].status);
-      }
-
-      var $table = $("<table>");
-      $table.addClass('table table-condensed');
-
-      var $header = $("<tr>");
-      $header.append($('<th>').html('Version'));
-      $header.append($('<th>').html('Date'));
-      $header.append($('<th>').html('Duration'));
-      $header.append($('<th>').html('Status'));
-      $header.append($('<th colspan="3">').html('Results'));
-      $table.append($header);
-
-      $.each(history, function(index, entry) {
-        var run_id = (entry.run_id || entry.date);
-        var debci_log = PACKAGES_DIR + '/' + pkg_dir + '/' + run_id  + ".log";
-        var adt_log = PACKAGES_DIR + '/' + pkg_dir + '/' + run_id  + ".autopkgtest.log";
-        var artifacts = AUTOPKGTEST_DIR + '/' + pkg_dir + '/' + run_id;
-
-        var $row = $('<tr>');
-        $row.append($('<td>').html(entry.version));
-        $row.append($('<td>').html(entry.date));
-        $row.append($('<td>').html(entry.duration_human));
-        $row.append($('<td>').html(entry.status).addClass(entry.status));
-        $row.append($('<td>').html($('<a>').attr('href', debci_log).html('debci log')));
-        $row.append($('<td>').html($('<a>').attr('href', adt_log).html('test log')));
-        $row.append($('<td>').html($('<a>').attr('href', artifacts).html('artifacts')));
-
-        $table.append($row);
-      });
-      $target.append($table);
-
-      var data_base = window.location.href.replace(/\/[#?].*/, '');
-      var automation_info =
-        "<p>Automate:</p>" +
-        "<pre><code>" +
-        "# latest status of the package\n" +
-        "$ curl " + data_base + "/" + PACKAGES_DIR + "/" + pkg_dir + '/latest.json\n' +
-        "\n" +
-        "# latest autopkgtest log of the package\n" +
-        "$ curl " + data_base + "/" + PACKAGES_DIR + "/" + pkg_dir + '/latest-autopkgtest/log\n' +
-        "\n" +
-        "# test run history of the package\n" +
-        "$ curl " + data_base + "/" + PACKAGES_DIR + "/" + pkg_dir + '/history.json\n' +
-        "</code></pre>";
-      $target.append(automation_info);
-
-    }).fail(function() {
-      $target.html(
-        '<h1 class="fail">Package <em>' + pkg + '</em> not found</h1>');
-    });
-  }
 
   $(window).trigger('hashchange');
 
