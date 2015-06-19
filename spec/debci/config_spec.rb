e8948662dcb2f4c3b391c765c451a438218bdd2b
Antonio Terceiro <terceiro@debian.org>
2015-06-19 13:23:06 -0300

    Debci::Config: make sure empty config vars are read as nil

--- spec/debci/config_spec.rb
+++ spec/debci/config_spec.rb
@@ -7,8 +7,8 @@ describe Debci::Config do
   KEYS = Debci::Config.members
 
   before(:each) do
-    io = StringIO.new(KEYS.map { |k| "#{k}=value-of-#{k}\n" }.join)
-    expect(IO).to receive(:popen).with(['debci', 'config', *KEYS.map(&:to_s)]).and_yield(io)
+    @io = StringIO.new(KEYS.map { |k| "#{k}=value-of-#{k}\n" }.join)
+    expect(IO).to receive(:popen).with(['debci', 'config', *KEYS.map(&:to_s)]).and_yield(@io)
   end
 
   KEYS.each do |key|
@@ -18,6 +18,12 @@ describe Debci::Config do
     it "strips newlines off #{key}" do
       expect(config.send(key)).to_not end_with("\n")
     end
+  end
+
+  it 'reads entry set to empty string in config file as nil' do
+    @io.reopen("data_basedir=/path/to/data\nartifacts_url_base=\n")
+    expect(config.artifacts_url_base).to be_nil
+    expect(config.data_basedir).to eq('/path/to/data')
   end
 
 end
