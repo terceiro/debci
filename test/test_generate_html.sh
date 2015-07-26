25d3344aab26227481df89ff19961fb357328ca7
Antonio Terceiro <terceiro@debian.org>
2015-07-26 12:15:36 -0300

    debci-generate-html: really fix running against an empty data directory
    Checking if the data directory is strictly empty will never work when
    installed as a Debian package, because there will be always a
    `schema_version` file there.

---
+++ test/test_generate_html.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+. $(dirname $0)/test_helper.sh
+
+test_almost_empty_data_dir() {
+  echo "1" > "$debci_data_basedir/schema_version"
+  rc=0
+  debci generate-html || rc=$?
+  assertEquals 0 $rc
+}
+
+. shunit2
