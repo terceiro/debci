496272d93028050d1f6a2065336d29ef56db9fca
Antonio Terceiro <terceiro@debian.org>
2016-07-28 18:24:04 -0300

    tools/gen-fake-data.sh: schedule tests on multiple suites

--- tools/gen-fake-data.sh
+++ tools/gen-fake-data.sh
@@ -10,8 +10,10 @@ if [ ! -f $configdir/whitelist ]; then
   done > $configdir/whitelist
 fi
 
-for arch in $(./bin/debci config --values-only arch_list); do
-  for pkg in $(cat $configdir/whitelist); do
-    ./bin/debci enqueue --arch "$arch" "$pkg"
+for suite in $(./bin/debci config --values-only suite_list); do
+  for arch in $(./bin/debci config --values-only arch_list); do
+    for pkg in $(cat $configdir/whitelist); do
+      ./bin/debci enqueue --arch="$arch" --suite="$suite" "$pkg"
+    done
   done
 done
