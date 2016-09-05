12f9ee333351e34b9c86d002963c273a158a69a7
Antonio Terceiro <terceiro@debian.org>
2016-09-05 11:14:26 -0300

    tools/gen-fake-data.sh: bootstrap development settings

--- tools/gen-fake-data.sh
+++ tools/gen-fake-data.sh
@@ -10,6 +10,11 @@ if [ ! -f $configdir/whitelist ]; then
   done > $configdir/whitelist
 fi
 
+if [ ! -f $configdir/conf.d/dev.conf ]; then
+  echo "debci_arch_list='amd64 arm64'" > $configdir/conf.d/dev.conf
+  echo "debci_backend=fake" > $configdir/conf.d/dev.conf
+fi
+
 for suite in $(./bin/debci config --values-only suite_list); do
   for arch in $(./bin/debci config --values-only arch_list); do
     for pkg in $(cat $configdir/whitelist); do
