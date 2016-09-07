2608fb6f60cd0b32a3e6b468e41aec98dfa08753
Antonio Terceiro <terceiro@debian.org>
2016-09-07 17:44:35 -0300

    tools/gen-fake-data.sh: fix dev.conf creation/also set debci_suite_list

--- tools/gen-fake-data.sh
+++ tools/gen-fake-data.sh
@@ -12,7 +12,8 @@ fi
 
 if [ ! -f $configdir/conf.d/dev.conf ]; then
   echo "debci_arch_list='amd64 arm64'" > $configdir/conf.d/dev.conf
-  echo "debci_backend=fake" > $configdir/conf.d/dev.conf
+  echo "debci_suite_list='unstable stable'" >> $configdir/conf.d/dev.conf
+  echo "debci_backend=fake" >> $configdir/conf.d/dev.conf
 fi
 
 for suite in $(./bin/debci config --values-only suite_list); do
