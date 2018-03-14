7fd7bb3d234ce200e748341941bf1c6c5ad7a09f
Antonio Terceiro <terceiro@debian.org>
2018-03-14 14:21:30 -0300

    tools: split development config setup into its own script

--- tools/gen-fake-data.sh
+++ tools/gen-fake-data.sh
@@ -2,19 +2,9 @@
 
 set -eu
 
-configdir=`$(dirname $0)/../bin/debci config --values-only config_dir`
+./tools/init-dev.sh
 
-if [ ! -f $configdir/whitelist ]; then
-  for pkg in ruby-defaults rubygems-integration autodep8 pristine-tar; do
-    echo "$pkg"
-  done > $configdir/whitelist
-fi
-
-if [ ! -f $configdir/conf.d/dev.conf ]; then
-  echo "debci_arch_list='amd64 arm64'" > $configdir/conf.d/dev.conf
-  echo "debci_suite_list='unstable stable'" >> $configdir/conf.d/dev.conf
-  echo "debci_backend=fake" >> $configdir/conf.d/dev.conf
-fi
+configdir=`./bin/debci config --values-only config_dir`
 
 for suite in $(./bin/debci config --values-only suite_list); do
   for arch in $(./bin/debci config --values-only arch_list); do
