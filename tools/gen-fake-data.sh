b8ca4386a306ff3df64479dea47b534dec285fdc
Antonio Terceiro <terceiro@debian.org>
2018-05-07 22:28:07 -0300

    tools/gen-fake-data.sh: optimize looping through whitelist
    Also, consume at most 50 packages from the whitelist, in random order.

--- tools/gen-fake-data.sh
+++ tools/gen-fake-data.sh
@@ -8,8 +8,7 @@ configdir=`./bin/debci config --values-only config_dir`
 
 for suite in $(./bin/debci config --values-only suite_list); do
   for arch in $(./bin/debci config --values-only arch_list); do
-    for pkg in $(cat $configdir/whitelist); do
-      ./bin/debci enqueue --arch="$arch" --suite="$suite" "$pkg"
-    done
+    shuf --head-count=50 config/whitelist \
+      | xargs ./bin/debci enqueue --arch="$arch" --suite="$suite"
   done
 done
