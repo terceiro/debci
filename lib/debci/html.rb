4b3ed7532b160d5a854e5aa21f902b36b9151fed
Antonio Terceiro <terceiro@debian.org>
2018-05-23 21:38:20 -0300

    Use FontAwesome via its standard API
    This stops debci from breaking when fonts-font-awesome is upgraded from
    v4 to v5, due to debci icorrectly using FontAwesome internals.

--- lib/debci/html.rb
+++ lib/debci/html.rb
@@ -135,6 +135,23 @@ module Debci
       text && CGI.escapeHTML(text)
     end
 
+    ICONS = {
+      pass: 'thumbs-up',
+      fail: 'thumbs-down',
+      fail_passed_never: ['thumbs-down', 'ban'],
+      fail_passed_current: ['thumbs-down', 'bolt'],
+      fail_passed_old: ['thumbs-down', 'arrow-down'],
+      tmpfail_pass: 'thumbs-up',
+      tmpfail_fail: 'thumbs-down',
+      tmpfail: 'question-circle',
+      no_test_data: 'question',
+    }
+    def icon(status)
+      Array(ICONS[status.to_sym]).map do |i|
+        "<i class='#{status} fa fa-#{i}'></i>"
+      end.join(' ')
+    end
+
   end
 
 end
