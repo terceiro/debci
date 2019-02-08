367fef619eceebc142d32d6630123da6199c55d9
Antonio Terceiro <terceiro@debian.org>
2019-02-08 20:50:16 -0200

    tools/indexer.sh: rewrite using rerun
    It used to use rerun before, I'm not sure why it was changed.

--- tools/indexer.sh
+++ tools/indexer.sh
@@ -6,15 +6,9 @@ incoming=`$(dirname $0)/../bin/debci config --values-only autopkgtest_incoming_b
 
 mkdir -p "$incoming"
 
-while true; do
-  if inotifywait \
-    --event modify \
-    --event create \
-    --timeout 1 \
-    --recursive \
-    --quiet --quiet \
-    "$incoming"; then
-    echo "I: changes in incoming directory; updating"
-    ./bin/debci update
-  fi
-done
+rerun \
+  --no-notify \
+  --exit \
+  --dir "$incoming" \
+  --pattern '**/exitcode' \
+  -- ./bin/debci update
