140e2f60fc215a6724693c3a18413c591590c6f0
bhavy singh <bhavys@iitk.ac.in>
2019-04-08 18:11:22 +0000

    spec/debci/config_spec.rb: fix Ruby code style

--- spec/debci/config_spec.rb
+++ spec/debci/config_spec.rb
@@ -1,7 +1,6 @@
 require 'debci/config'
 
 describe Debci::Config do
-
   let(:config) { Debci::Config.new }
 
   KEYS = Debci::Config.members
@@ -37,9 +36,8 @@ describe Debci::Config do
   end
 
   it 'splits at the first equal sign only' do
-    url = "sqlite3:/path/to/db.sqlite3?timeout=5000"
+    url = 'sqlite3:/path/to/db.sqlite3?timeout=5000'
     @io.reopen("database_url=#{url}\n")
     expect(config.database_url).to eq(url)
   end
-
 end
