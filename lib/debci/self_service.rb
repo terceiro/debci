f704ec408ede6822734aae0f5751528edb421952
Candy Tsai <candy02058912@gmail.com>
2019-07-08 17:38:45 +0800

    self_service: run tests from form submission

--- lib/debci/self_service.rb
+++ lib/debci/self_service.rb
@@ -21,5 +21,59 @@ module Debci
     get '/' do
       erb :self_service
     end
+
+    get '/test' do
+      erb :self_service_test
+    end
+
+    post '/test/submit' do
+      trigger = params[:trigger] || ''
+      package = params[:package] || ''
+      suite = params[:suite] || ''
+      archs = (params[:arch] || []).reject(&:empty?)
+      pin_packages = (params[:pin_packages] || '').split(/\n+|\r+/).reject(&:empty?)
+
+      # assemble test request
+      test_obj = {
+        'trigger' => trigger,
+        'package' => package
+      }
+      test_obj['pin-packages'] = []
+      pin_packages.each do |pin_package|
+        pin_package = pin_package.split(/,\s*/)
+        test_obj['pin-packages'].push(pin_package)
+      end
+      test_request = {
+        'archs' => archs,
+        'suite' => suite,
+        'tests' => [test_obj]
+      }
+
+      # validate inputs
+      begin
+        validate_form_submission(package, suite, archs)
+        archs.each do |arch|
+          request_tests(test_request['tests'], suite, arch, @user)
+        end
+      rescue StandardError => error
+        @error_msg = error
+        halt(400, erb(:self_service_test))
+      end
+
+      # user clicks on export to json
+      if params[:export]
+        content_type :json
+        [200, [test_request].to_json]
+      # user submits test
+      else
+        201
+      end
+    end
+
+    def validate_form_submission(package, suite, archs)
+      raise 'Please enter a valid package name' unless valid_package_name?(package)
+      raise 'Please select a suite' if suite == ''
+      raise 'Please select an architecture' if archs.empty?
+    end
   end
 end
--- spec/debci/self_service_spec.rb
+++ spec/debci/self_service_spec.rb
@@ -30,4 +30,49 @@ describe Debci::SelfService do
       expect(last_response.content_type).to match('text/html')
     end
   end
+
+  context 'request test form' do
+    it 'exports a json file successfully from test form' do
+      post '/selfservice/test/submit', { pin_packages: '', trigger: 'test_trigger', package: 'test-package', suite: suite, arch: [arch], export: true }, 'SSL_CLIENT_S_DN_CN' => 'foo@bar.com'
+      expect(last_response.status).to eq(200)
+      expect(last_response.content_type).to match('application/json')
+    end
+
+    it 'should return error when exporting a json file from incomplete test form' do
+      post '/selfservice/test/submit', { pin_packages: '', trigger: '', package: '', suite: suite, arch: [arch], export: true }, 'SSL_CLIENT_S_DN_CN' => 'foo@bar.com'
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'submits a task succesfully from the form' do
+      post '/selfservice/test/submit', { pin_packages: '', trigger: 'test_trigger', package: 'test-package', suite: suite, arch: [arch] }, 'SSL_CLIENT_S_DN_CN' => 'foo@bar.com'
+      expect(last_response.status).to eq(201)
+      job = Debci::Job.last
+      expect(job.package).to eq('test-package')
+      expect(job.trigger).to eq('test_trigger')
+      expect(job.arch).to eq(arch)
+      expect(job.suite).to eq(suite)
+      expect(job.pin_packages).to eq([])
+    end
+
+    it 'should return error when submitting form with empty package field' do
+      job_count = Debci::Job.count
+      post '/selfservice/test/submit', { pin_packages: '', trigger: 'test_trigger', package: '', suite: suite, arch: [arch] }, 'SSL_CLIENT_S_DN_CN' => 'foo@bar.com'
+      expect(Debci::Job.count).to eq(job_count)
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'should return error when submitting form with empty suite field' do
+      job_count = Debci::Job.count
+      post '/selfservice/test/submit', { pin_packages: '', trigger: 'test_trigger', package: 'test-package', suite: '', arch: [arch] }, 'SSL_CLIENT_S_DN_CN' => 'foo@bar.com'
+      expect(Debci::Job.count).to eq(job_count)
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'should return error when submitting form with empty arch field' do
+      job_count = Debci::Job.count
+      post '/selfservice/test/submit', { pin_packages: '', trigger: '', package: 'test-package', suite: suite, arch: [] }, 'SSL_CLIENT_S_DN_CN' => 'foo@bar.com'
+      expect(Debci::Job.count).to eq(job_count)
+      expect(last_response.status).to eq(400)
+    end
+  end
 end
