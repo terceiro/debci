3862b9750e259ae0d6e6ecfed86bcf3bf412c1ab
Antonio Terceiro <terceiro@debian.org>
2019-09-30 14:14:13 -0300

    ui: always escape triggers

--- lib/debci/app.rb
+++ lib/debci/app.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
+require 'erb'
 
 module Debci
   class App < Sinatra::Base
+    include ERB::Util
     before do
       @user = ENV['FAKE_CERTIFICATE_USER'] || env['SSL_CLIENT_S_DN_CN']
     end
