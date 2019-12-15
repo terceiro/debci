18024a7203414ddc201a90e0b1cc317526c6a387
Antonio Terceiro <terceiro@debian.org>
2019-12-15 11:45:27 -0300

    debci-test: drop testbed locking

--- lib/environment.sh
+++ lib/environment.sh
@@ -164,7 +164,6 @@ debci_amqp_server_display="$(echo "$debci_amqp_server" | sed -e 's#:[^/]*@#:****
 debci_lock_dir=${debci_lock_dir:-/var/lock}
 
 # per-suite/architecture lock/timestamp files
-debci_testbed_lock=${debci_lock_dir}/debci-testbed-${debci_suite}-${debci_arch}-${debci_backend}.lock
 debci_testbed_timestamp=${debci_lock_dir}/debci-testbed-${debci_suite}-${debci_arch}-${debci_backend}.stamp
 debci_chdist_lock=${debci_lock_dir}/debci-chdist-${debci_suite}-${debci_arch}.lock
 debci_batch_lock=${debci_lock_dir}/debci-batch-${debci_suite}-${debci_arch}.lock
