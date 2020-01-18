bd055b34430dcd3a33da54cb63bb484b0321a25b
Antonio Terceiro <terceiro@debian.org>
2020-01-18 14:04:11 -0300

    debci-localtest: ignore failure to record package
    When debci tests are running under debci itself, /var/tmp/debci.pkg is
    taken by the outer autopkgtest binary.

--- test/test_localtest.sh
+++ test/test_localtest.sh
@@ -5,7 +5,10 @@
 fake_package="${0%/*}/fake-package"
 
 test_basic() {
-  assertTrue "debci localtest -b null $fake_package"
+  rc=0
+  output=$(debci localtest -b null "$fake_package" 2>&1) || rc=$?
+  assertEquals 0 "$rc"
+  if [ "$rc" -ne 0 ]; then echo "$output"; fi
 }
 
 . shunit2
