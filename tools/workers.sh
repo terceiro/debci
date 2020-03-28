3a566b41082bf0571c9f106ff5c8f9e3f86d6142
Antonio Terceiro <terceiro@debian.org>
2020-03-28 15:39:17 -0300

    tools/workers.sh: assume GNU parallel

--- tools/workers.sh
+++ tools/workers.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-parallel ./bin/debci worker --arch -- $(./bin/debci config --values-only arch_list)
+parallel --ungroup \
+	./bin/debci worker --arch \
+		::: $(./bin/debci config --values-only arch_list)
