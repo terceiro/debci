4364fdd31f97d28b4ad81bb277bcb296f39a274c
Antonio Terceiro <terceiro@debian.org>
2020-04-21 11:20:41 -0300

    tools/gen-fake-data.sh: allow to specify packages
    If no packages are specified, the previous behavior of taking up to 50
    packages from the whitelist applies.
    
    Gbp-Dch: ignore

--- tools/gen-fake-data.sh
+++ tools/gen-fake-data.sh
@@ -4,11 +4,18 @@ set -eu
 
 ./tools/init-dev.sh
 
+get_packages() {
+  if [ $# -eq 0 ]; then
+    set -- $(shuf --head-count=50 config/whitelist)
+  fi
+  echo "$@"
+}
+
 configdir=`./bin/debci config --values-only config_dir`
 
 for suite in $(./bin/debci config --values-only suite_list); do
   for arch in $(./bin/debci config --values-only arch_list); do
-    shuf --head-count=50 config/whitelist \
+    get_packages "$@" \
       | xargs ./bin/debci enqueue --arch="$arch" --suite="$suite"
   done
 done
