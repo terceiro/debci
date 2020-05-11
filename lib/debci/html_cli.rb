f8d588ea5b5f105d00098776c8ef9b31378763d2
Antonio Terceiro <terceiro@debian.org>
2020-05-11 20:44:10 -0300

    debci html: fix update-package

--- lib/debci/html_cli.rb
+++ lib/debci/html_cli.rb
@@ -1,5 +1,6 @@
 require 'thor'
 require 'debci/html'
+require 'debci/package'
 
 module Debci
   class HTML
@@ -11,7 +12,8 @@ module Debci
 
       desc 'update-package PACKAGE [SUITE] [ARCHITECTURE]', 'Updates HTML for a given package, optionally only for the given suite and architecture'
       def update_package(pkg, suite = nil, arch = nil)
-        Debci::HTML.update_package(pkg, suite, arch)
+        package = Debci::Package.where(name: pkg).first
+        Debci::HTML.update_package(package, suite, arch)
       end
     end
   end
