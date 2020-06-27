terceiro/debci

commit 3b9b087a7f786f19a3960a569250ba6aca3eb53b

Add a proper user model

lib/debci/self_service.rb
@@ -36,14 +36,14 @@ module Debci
     end
 
     get '/' do
-      redirect("/user/#{@user}")
+      redirect("/user/#{@user.username}")
     end
 
     get '/login' do
       user = read_request_user
       if user
         session[:user] = user
-        redirect("/user/#{user}")
+        redirect("/user/#{user.username}")
       else
         content = "Unauthenticated!\n"
         content = File.read(Debci.config.auth_fail_page) if Debci.config.auth_fail_page
@@ -57,7 +57,7 @@ module Debci
     end
 
     get '/:user' do
-      redirect("/user/#{params[:user]}/jobs") unless @user == params[:user]
+      redirect("/user/#{params[:user]}/jobs") unless @user.username == params[:user]
       erb :self_service
     end
 

lib/debci/api.rb
@@ -9,7 +9,7 @@ require 'time'
 require 'debci'
 require 'debci/app'
 require 'debci/job'
-require 'debci/key'
+require 'debci/user'
 require 'debci/test_handler'
 
 
@@ -233,7 +233,7 @@ module Debci
       EOF
       get '/test' do
         authenticate_key!
-        jobs = Debci::Job.where(requestor: @user)
+        jobs = Debci::Job.where(requestor: @user.username)
         if params[:since]
           since = Time.strptime(params[:since], '%s')
           jobs = jobs.where('updated_at >= ?', since)
@@ -380,7 +380,7 @@ module Debci
             package: package,
             suite: params[:suite],
             arch: params[:arch],
-            requestor: @user,
+            requestor: @user.username,
         )
         self.enqueue(job, @priority)
 
@@ -409,7 +409,7 @@ module Debci
     def authenticate_key!
       key = env['HTTP_AUTH_KEY']
       if key && @user = Debci::Key.authenticate(key)
-        response['Auth-User'] = @user
+        response['Auth-User'] = @user.username
       else
         halt(403, "Invalid key\n")
       end

lib/debci/app.rb
@@ -1,11 +1,13 @@
 require 'sinatra'
 require 'erb'
+require 'debci/user'
 
 module Debci
   class App < Sinatra::Base
     include ERB::Util
     def read_request_user
-      ENV['FAKE_CERTIFICATE_USER'] || env['SSL_CLIENT_S_DN_CN']
+      username = ENV['FAKE_CERTIFICATE_USER'] || env['SSL_CLIENT_S_DN_CN']
+      Debci::User.find_or_create_by!(username: username) if username
     end
   end
 end

lib/debci/db/migrations/20200515000000_create_users.rb
@@ -1,0 +1,35 @@
+class CreateUsers < Debci::DB::LEGACY_MIGRATION
+  def up
+    create_table :users do |t|
+      t.string :username, limit: 256, null: false, unique: true
+      t.boolean :admin, default: false
+    end
+
+    add_column :keys, :user_id, :integer, null: true
+    add_foreign_key :keys, :users
+    execute <<~SQL
+    INSERT INTO users (username)
+    SELECT DISTINCT("user") from keys;
+
+    UPDATE keys SET user_id = users.id
+    FROM users
+    WHERE users.username = keys.user;
+    SQL
+    change_column_null :keys, :user_id, false
+    remove_column :keys, :user
+  end
+
+  def down
+    add_column :keys, :user, :string, limit: 256, null: true
+    execute <<~SQL
+    UPDATE keys
+    SET "user" = users.username
+    FROM users
+    WHERE users.id = keys.user_id
+    SQL
+    remove_foreign_key :keys, :users
+    change_column_null :keys, :user, false
+    remove_column :keys, :user_id
+    drop_table :users
+  end
+end

lib/debci/html/templates/self_service.erb
@@ -1,5 +1,5 @@
 <ol class="breadcrumb">
-  <li class="active">Welcome <%= @user %>!</a></li>
+  <li class="active">Welcome <%= @user.username %>!</a></li>
 </ol>
 <header class="well">
   <h1>Self Service</h1>
@@ -7,7 +7,7 @@
 </header>
 <div class="row">
   <div class="col-xs-12 col-md-6">
-    <a class="well self-service-card text-center" href="/user/<%= @user %>/test">
+    <a class="well self-service-card text-center" href="/user/<%= @user.username %>/test">
       <div class="self-service-card-icon">
         <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>
       </div>
@@ -15,7 +15,7 @@
     </a>
   </div>
   <div class="col-xs-12 col-md-6">
-    <a class="well self-service-card text-center" href="/user/<%= @user %>/jobs">
+    <a class="well self-service-card text-center" href="/user/<%= @user.username %>/jobs">
       <div class="self-service-card-icon">
         <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
       </div>

lib/debci/html/templates/self_service_history.erb
@@ -1,6 +1,6 @@
-<% if @user == params[:user] %>
+<% if @user&.username == params[:user] %>
 <ol class="breadcrumb">
-  <li><a href="/user">Welcome <%= @user %>!</a></li>
+  <li><a href="/user">Welcome <%= @user.username %>!</a></li>
   <li class="active">Job History</li>
 </ol>
 <% end %>

lib/debci/html/templates/self_service_test.erb
@@ -1,5 +1,5 @@
 <ol class="breadcrumb">
-  <li><a href="/user">Welcome <%= @user %>!</a></li>
+  <li><a href="/user">Welcome <%= @user.username %>!</a></li>
   <li class="active">Request Test</li>
 </ol>
 <% if @error_msg %>
@@ -21,7 +21,7 @@
   <div class="page-header">
     <h3>Send Request by Uploading JSON File</h3>
   </div>
-  <form action="/user/<%= @user %>/test/upload" method="post" enctype="multipart/form-data">
+  <form action="/user/<%= @user.username %>/test/upload" method="post" enctype="multipart/form-data">
     <div class="form-group">
       <input name="tests" type="file" id="submit-json">
       <p class="help-block">You can get an example JSON through exporting the filled-out form below or click <a
@@ -36,7 +36,7 @@
   <div class="page-header">
     <h3>Send Request by Form</h3>
   </div>
-  <form action="/user/<%= @user %>/test/submit" method="post">
+  <form action="/user/<%= @user.username %>/test/submit" method="post">
     <div class="row">
       <div class="col-xs-12 col-sm-6">
         <div class="form-group">

lib/debci/key.rb
@@ -5,6 +5,8 @@ require 'debci/db'
 
 module Debci
   class Key < ActiveRecord::Base
+    belongs_to :user, class_name: 'Debci::User'
+
     attr_accessor :key
 
     before_create do |key|
@@ -12,9 +14,9 @@ module Debci
       key.encrypted_key = key.class.encrypt(key.key)
     end
 
-    def self.reset!(username)
-      find_by(user: username)&.destroy
-      create!(user: username)
+    def self.reset!(user)
+      find_by(user: user)&.destroy
+      create!(user: user)
     end
 
     def self.authenticate(key)
@@ -22,7 +24,7 @@ module Debci
       entry&.user || nil
     end
 
-    # Since the key being encrypt is random, there is no point is using salts
+    # Since the key being encrypted is random, there is no point is using salts
     # to protect against rainbow tables. So let's just use a good old SHA1
     # hash.
     def self.encrypt(key)

commit 410f1b1f7a39859ffea41866c0a782ef46caa101

self service: fix "Request Test" against new user model
Broken by 3b9b087

Gbp-Dch: ignore

lib/debci/self_service.rb
@@ -61,7 +61,7 @@ module Debci
     end
 
     get '/:user/test' do
-      redirect("/user/#{params[:user]}/jobs") unless @user == params[:user]
+      redirect("/user/#{params[:user]}/jobs") unless @user.username == params[:user]
       erb :self_service_test
     end
 