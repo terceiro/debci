5efde41527d9e559cbb40957d6c340eb5918837f
Antonio Terceiro <terceiro@debian.org>
2020-10-21 11:39:37 -0300

    tools/vagrant_root.sh: make script idempotent

--- tools/vagrant_root.sh
+++ tools/vagrant_root.sh
@@ -2,7 +2,7 @@
 
 echo "Running as root"
 export DEBIAN_FRONTEND=noninteractive
-echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian buster-backports main" > /etc/apt/sources.list.d/backports.list
 apt-get -y update
 apt-get -qqyt buster-backports install autopkgtest
 apt-get -qqy install make ruby git debootstrap
