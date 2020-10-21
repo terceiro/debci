b0e01465057a805fd6460ae62f9372c14e3d63a0
Antonio Terceiro <terceiro@debian.org>
2020-10-21 11:29:18 -0300

    vagrant: upgrade to buster

--- tools/vagrant_root.sh
+++ tools/vagrant_root.sh
@@ -2,9 +2,9 @@
 
 echo "Running as root"
 export DEBIAN_FRONTEND=noninteractive
-echo "deb http://deb.debian.org/debian stretch-backports main" >> /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian buster-backports main" >> /etc/apt/sources.list
 apt-get -y update
-apt-get -qqyt stretch-backports install autopkgtest
+apt-get -qqyt buster-backports install autopkgtest
 apt-get -qqy install make ruby git debootstrap
 cd /vagrant
 apt-get -qqy build-dep .
