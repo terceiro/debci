065e514988dc3f17c74f9d1f24cb853346977757
Antonio Terceiro <terceiro@debian.org>
2021-04-04 09:34:23 -0300

    Debci::App: extract get_page_range from Debci::SelfService

--- lib/debci/app.rb
+++ lib/debci/app.rb
@@ -16,5 +16,17 @@ module Debci
       username = ENV['FAKE_CERTIFICATE_USER'] || env['SSL_CLIENT_S_DN_CN']
       Debci::User.find_or_create_by!(username: username) if username
     end
+
+    def self.get_page_range(current, total)
+      full_range = (1..total)
+      middle = ((current - 5)..(current + 5)).select { |i| full_range.include?(i) }
+      start = middle.include?(1) ? [] : [1, nil]
+      finish = middle.include?(total) ? [] : [nil, total]
+      start + middle + finish
+    end
+
+    def get_page_range(current, total)
+      self.class.get_page_range(current, total)
+    end
   end
 end
