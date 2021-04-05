1655f82e202dd048e7fc4fe70d38727e97fc9e3a
root <root@Kritika.localdomain>
2021-04-05 19:16:13 +0530

    fixed customize.sh

--- backends/qemu/customize.sh
+++ backends/qemu/customize.sh
@@ -2,18 +2,18 @@
 
 set -eu
 
-base=$(readlink -f $(dirname $(readlink -f $0))/../..)
-. $base/lib/environment.sh
-prepare_args
+base=$(readlink -f "$(dirname "$(readlink -f "$0")")/../..")
+. "$base/lib/environment.sh"
 
+prepare_args
 rootfs="$1"
 
 # determine whether it's Debian or Ubuntu
-script=/usr/share/debootstrap/scripts/$debci_suite
-if [ -r $script ]; then
-  if grep -q ubuntu.com $script; then
+script="/usr/share/debootstrap/scripts/${debci_suite:?}"
+if [ -r "$script" ]; then
+  if grep -q ubuntu.com "$script"; then
     distro=ubuntu
-  elif grep -q kali.org $script; then
+  elif grep -q kali.org "$script"; then
     distro=kali
   else
     distro=debian
@@ -38,12 +38,12 @@ fi
 
 # convert apt proxy pointing to the VM's host back to localhost
 proxy_opt=
-RES=`chroot "${rootfs}" apt-config shell PROXY Acquire::http::Proxy`
+RES=$(chroot "${rootfs}" apt-config shell PROXY Acquire::http::Proxy)
 eval "$RES"
 if [ -n "$PROXY" ]; then
   proxy_opt="-o Acquire::http::Proxy=$(echo "${PROXY}" | sed -e 's/10.0.2.2/127.0.0.1/')"
 fi
-
+# shellcheck disable=SC2086
 DEBIAN_FRONTEND=noninteractive \
   chroot "$rootfs"  \
   apt-get install $proxy_opt dpkg-dev ca-certificates -q -y --no-install-recommends
