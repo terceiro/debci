2aba1e39ff6183578847b273b15e5e1b787ab719
Antonio Terceiro <terceiro@debian.org>
2021-06-27 09:28:45 -0300

    AddRequestorIdToJobs: avoid joins during update
    This reduces the migration time against the production database
    (~ 13,000,000 jobs) from ~20 to ~8 minutes

--- lib/debci/db/migrations/20210612000000_add_requestor_id_to_jobs.rb
+++ lib/debci/db/migrations/20210612000000_add_requestor_id_to_jobs.rb
@@ -3,7 +3,10 @@ class AddRequestorIdToJobs < Debci::DB::LEGACY_MIGRATION
     add_column :jobs, :requestor_id, :integer
     add_foreign_key :jobs, :users, column: :requestor_id
     execute "INSERT INTO users (username) SELECT DISTINCT requestor FROM jobs WHERE requestor NOT IN (SELECT username FROM users)"
-    execute "UPDATE jobs SET requestor_id = (SELECT users.id FROM users WHERE jobs.requestor = users.username)"
+    cases = select_all("SELECT * FROM users").map do |u|
+      "WHEN requestor = '#{u['username']}' THEN #{u['id']}"
+    end.join(" ")
+    execute "UPDATE jobs SET requestor_id = CASE #{cases} END" unless cases.empty?
     change_column_null :jobs, :requestor_id, false
     remove_column :jobs, :requestor
   end
